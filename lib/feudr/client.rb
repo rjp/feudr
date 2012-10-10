9078e2fb97c64bc33926417b901b5fbcb26dca26
Paul Brannan <curlypaul924@gmail.com>
2012-10-10 15:17:08 -0400

    Fixed some typos, added login (doesn't work yet)

--- lib/feudr/client.rb
+++ lib/feudr/client.rb
@@ -4,6 +4,8 @@ require 'json'
 module Feudr
 
 class Client
+  class Error < RuntimeError; end
+
   RuleNames = [
     'American',
     'Norwegian',
@@ -16,18 +18,53 @@ class Client
   ]
 
   def initialize
-    @serverid = '%02d' % random(7)
+    @serverid = '%02d' % rand(7)
     @urlbase = "http://game#{@serverid}.wordfeud.com/wf"
   end
 
   def post(location, body = [ ])
-    uri = "#{@urlbase}/location"
+    uri = "#{@urlbase}/#{location}"
+    p uri
     json = body.to_json
-    RestClient.post(uri, json, :content_type => 'application/json')
+    p json
+    return RestClient.post(uri, json, :content_type => 'application/json')
+  end
+
+  def login(userid, password)
+    result = post(
+        'user/login/',
+        'id'        => userid,
+        'password'  => password)
+
+    if result['status'] != 'success' then
+      raise Error, "Could not login: #{result['content']['message']}"
+    end
+  end
+
+  def login_with_email(email, password)
+    result = post(
+        'user/login/',
+        'email'     => email,
+        'password'  => password)
+
+    if result['status'] != 'success' then
+      p result
+      raise Error, "Could not login: #{result['content']['message']}"
+    end
   end
 
   def check_notifications
     post('user/notifications')
+  end
+
+  def user_status
+    post('user/status')
+  end
+
+  def user_search(username_or_email)
+    post(
+        'user/search',
+        'username_or_email' => username_or_email)
   end
 
   def resume
@@ -51,7 +88,6 @@ class Client
         'random_request/create',
         'ruleset'   => ruleset,
         'boardtype' => boardtype)
-    }
   end
 
   def resign(game)
